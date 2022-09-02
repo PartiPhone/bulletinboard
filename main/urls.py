89f600438bb322bb9a5f122e91274e4b87be8277
User <user@example.com>
2022-09-02 17:43:16 +0500

    Создание вспомогательной веб-страницы (О сайте)

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, other_page
 
 app_name = 'main'
 urlpatterns = [
-    path('', index, name='index')
-    
+    path('<str:page>/', other_page, name='other'),
+    path('', index, name='index'),
 ]
 
