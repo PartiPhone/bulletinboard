00eb68bb86c712687edeb7efa86aab835015f247
User <user@example.com>
2022-09-03 14:37:06 +0500

    Создал модель пользователя и пофиксил выпадающий список

--- bboard/settings.py
+++ bboard/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
+    'django.contrib.postgres',
     'main.apps.MainConfig',
 ]
 
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main.AdvUser'
+
