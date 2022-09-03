2a3c09bf6ceacf35c21425dca99c1ec8299f2ac3
User <user@example.com>
2022-09-03 15:51:10 +0500

    Добавление страницы профиля и выхода

--- main/views.py
+++ main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     return render(request, 'main/index.html')
@@ -17,3 +19,10 @@ def other_page(request, page):
     
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
+    
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+    
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
