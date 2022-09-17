26d4e7567ce1e94932077712d58658de70f8e3fe
User <user@example.com>
2022-09-17 12:29:41 +0500

    Реализация отправки уведомления о добавлении нового комментария

--- main/models.py
+++ main/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
-from .utilities import get_timestamp_path
-
+from .utilities import get_timestamp_path, send_new_comment_notification
 
 class AdvUser(AbstractUser):
     is_activated = models.BooleanField(default=True, db_index=True,
@@ -110,3 +110,10 @@ class Comment(models.Model):
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
         ordering = ('created_at',)
+
+def post_save_dispatcher(sender, **kwargs):
+    author = kwargs['instance'].bb.author
+    if kwargs['created'] and author.send_messages:
+        send_new_comment_notification(kwargs['instance'])
+
+post_save.connect(post_save_dispatcher, sender=Comment)
