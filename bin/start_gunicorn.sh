aaf60b6a756d9fe7f3d6d09506d7e352efd8b6c4
User <user@example.com>
2022-11-29 13:12:11 +0500

    Пару фиксов

--- bin/start_gunicorn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /home/user/проекты/testproject/venv/bin/activate
-exec gunicorn -c /home/user/проекты/testproject/bboard/config/gunicorn/dev.py 
--- config/gunicorn/dev.py
+++
@@ -1,8 +0,0 @@
-wsgi_app = 'bboard.wsgi:application'
-workers = 2
-loglevel = 'debug'
-capture_output = True
-limit_request_fields = 32000
-limit_request_field_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=siteparser.settings'
-daemon = True
